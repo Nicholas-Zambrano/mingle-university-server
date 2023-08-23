aa96ffd8b30ebcc74649f6b403e2719009b8a4db
Nicholas Zambrano <nicholaszambrano@hotmail.co.uk>
2023-08-23 15:35:42 +0100

    added the migrations

--- knexfile.js
+++ knexfile.js
@@ -8,8 +8,9 @@ module.exports = {
   client: "mysql",
   connection: {
     host: process.env.DB_HOST,
-    database: process.env.DB_NAME,
+    database: process.env.DB_DATABASE,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
+    charset: "utf8",
   },
 };
---
+++ migrations/20230823133645_create_users_table.js
@@ -0,0 +1,17 @@
+exports.up = function (knex) {
+  return knex.schema.createTable("users", (table) => {
+    table.increments("id").primary();
+    table.string("first_name").notNullable();
+    table.string("last_name").notNullable();
+    table.string("address");
+    table.string("phone");
+    table.string("email").notNullable().unique();
+    table.string("password").notNullable();
+    table.string("hobby").notNullable();
+    table.timestamps(true, true)
+  });
+};
+
+exports.down = function (knex) {
+  return knex.schema.dropTable("users");
+};
